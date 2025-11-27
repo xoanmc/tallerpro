d03a33821dacde468b151fe59538646057b77a47
Xoan <xoanmc@hotmail.com>
2025-11-27 12:08:52 +0100

    fix(index, main): correct imports and routes

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '../app/router';
-import './index.css';
-import './styles/globals.css';
+import '../assets/styles/index.css';
+import '../assets/styles/global.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
